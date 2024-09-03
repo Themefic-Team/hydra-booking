a650fd3e41ce415b33f69899239ee6e89bf001d8
mdrejon <sydurrahmant1@gmail.com>
2024-09-03 16:25:55 +0600

    update push origin

--- src/view/booking/booking-create.vue
+++ src/view/booking/booking-create.vue
@@ -38,7 +38,7 @@ const flatpickr_date= reactive({
 }); 
 const fetchPreBookingData = async () => {
     try { 
-        const response = await axios.get(tfhb_core_apps.admin_url + '/wp-json/hydra-booking/v1/booking/pre'{
+        const response = await axios.get(tfhb_core_apps.admin_url + '/wp-json/hydra-booking/v1/booking/pre', {
             headers: {
                 'X-WP-Nonce': tfhb_core_apps.rest_nonce,
                 'capability': 'tfhb_manage_options'
