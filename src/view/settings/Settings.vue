c0ef87d92898dbeea782af6e4daa80abb6f8fc4e
mdrejon <sydurrahmant1@gmail.com>
2024-10-18 00:15:53 +0600

    settings tab possition

--- src/view/settings/Settings.vue
+++ src/view/settings/Settings.vue
@@ -14,7 +14,7 @@ import Header from '@/components/Header.vue';
             <nav class="tfhb-hydra-admin-tabs tfhb-hydra-settings">  
                 <ul>
                     <li><router-link to="/settings/general" exact :class="{ 'active': $route.path === '/settings/general' }"> <Icon name="SlidersHorizontal" /> {{ __('General', 'hydra-booking') }}</router-link></li> 
-                    <li><router-link to="/settings/hosts-settings" exact :class="{ 'active': $route.path.startsWith('/settings/hosts-settings') }"> <Icon name="UserCog" /> {{ __('Host Settings', 'hydra-booking') }}</router-link></li> 
+                   
                     <li><router-link to="/settings/availability" :class="{ 'active': $route.path === '/settings/availability' }"> <Icon name="Clock" /> {{ __('Availability', 'hydra-booking') }}</router-link></li> 
                     <li><router-link to="/settings/notifications" :class="{ 'active': $route.path === '/settings/notifications' }"> <Icon name="BellDot" /> {{ __('Notifications', 'hydra-booking') }}</router-link></li>
                     <li  :class="{ 'expand': $route.path === '/settings/integrations' }" class="tfhb-integrations-settings-menu"><router-link to="/settings/integrations#all" class="integrations-submenu" data-filter="all" :class="{ 'active': $route.path === '/settings/integrations' }"> <Icon name="Unplug" /> {{ __('Integrations', 'hydra-booking') }}
@@ -37,7 +37,7 @@ import Header from '@/components/Header.vue';
                     <li><router-link to="/settings/appearance" :class="{ 'active': $route.path === '/settings/appearance' }"> <Icon name="SwatchBook" /> {{ __('Appearance', 'hydra-booking') }}</router-link></li>
 
                     <li><router-link to="/settings/category" :class="{ 'active': $route.path === '/settings/category' }"> <Icon name="ClipboardList" /> {{ __('Meeting Category', 'hydra-booking') }}</router-link></li>
-                     
+                    <li><router-link to="/settings/hosts-settings" exact :class="{ 'active': $route.path.startsWith('/settings/hosts-settings') }"> <Icon name="UserCog" /> {{ __('Host Settings', 'hydra-booking') }}</router-link></li> 
                 </ul>  
             </nav>  
             <div class="tfhb-hydra-dasboard-content"> 
