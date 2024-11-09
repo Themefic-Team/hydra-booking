59fa595c0f7dd838583429a2e4ecae8c4d25513e
Sydur Rahman <sydurrahmant1@gmail.com>
2024-11-09 21:30:18 +0600

    update plugins

--- src/store/dateformat.js
+++ src/store/dateformat.js
@@ -1,17 +1,21 @@
 export default function useDateFormat() {
 
-    const Tfhb_Date = (dateString) => { 
-        const dates = dateString.split(','); 
+    const Tfhb_Date = (dateString) => {
+        const options = { day: 'numeric', month: 'long', year: 'numeric' };
+        const date = new Date(dateString);
+        return date.toLocaleDateString('en-US', options);
 
-        // make array of date objects
-        const dateObjects = dates.map(date => {
-            const options = { day: 'numeric', month: 'long', year: 'numeric' };
-            const dateObject = new Date(date);
-            return dateObject.toLocaleDateString('en-US', options);
-        });
-        // make array to string
-        return dateObjects.join(', ');
-        console.log('dateObjects', dateObjects);
+        // const dates = dateString.split(','); 
+
+        // // make array of date objects
+        // const dateObjects = dates.map(date => {
+        //     const options = { day: 'numeric', month: 'long', year: 'numeric' };
+        //     const dateObject = new Date(date);
+        //     return dateObject.toLocaleDateString('en-US', options);
+        // });
+        // // make array to string
+        // return dateObjects.join(', ');
+        // console.log('dateObjects', dateObjects);
     }
 
     const Tfhb_Time = (timeString) => {
