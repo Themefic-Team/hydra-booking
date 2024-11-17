860868878d4984cc2ed5a9d27761b2bd27dd7834
mdrejon <sydurrahmant1@gmail.com>
2024-11-17 17:03:49 +0600

    = 1.0.5 - 17/11/2024 =

--- hydra-booking.php
+++ hydra-booking.php
@@ -3,7 +3,7 @@
  * Plugin Name: Hydra Booking
  * Plugin URI: https://hydrabooking.com/
  * Description: Create a booking / Appointment Form using Contact Form 7. You can insert Calendar, Time on the form and manage your booking. User can pay using WooCommerce.
- * Version: 1.0.3
+ * Version: 1.0.5
  * Author: Themefic
  * Author URI: https://themefic.com/
  * License: GPL-2.0+
