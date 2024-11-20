8f0272c843965d8575ecbd79a4ea69902742202d
mdrejon <sydurrahmant1@gmail.com>
2024-11-20 17:10:32 +0600

    update build

--- admin/Controller/Enqueue.php
+++ admin/Controller/Enqueue.php
@@ -41,11 +41,11 @@ class Enqueue {
 		wp_enqueue_style( 'tfhb-admin-style', THB_URL . 'assets/admin/css/tfhb-admin-style.css', array(), null );
 
 		
-		wp_enqueue_script( 'tfhb-admin-core', apply_filters('tfhb_admin_core_script', 'http://localhost:5173/src/main.js'), array(), time(), true );
+		// wp_enqueue_script( 'tfhb-admin-core', apply_filters('tfhb_admin_core_script', 'http://localhost:5173/src/main.js'), array(), time(), true );
 
 		//  Build the core script
-		// wp_enqueue_script('tfhb-admin-core',  apply_filters('tfhb_admin_core_script', THB_URL .'build/assets/tfhb-admin-app-script.js'), [], time(), true); 
-		// wp_enqueue_style('tfhb-admin-style-core',  apply_filters('tfhb_admin_core_style', THB_URL .'build/assets/tfhb-admin-app.css'), [], time(), 'all');
+		wp_enqueue_script('tfhb-admin-core',  apply_filters('tfhb_admin_core_script', THB_URL .'build/assets/tfhb-admin-app-script.js'), [], time(), true); 
+		wp_enqueue_style('tfhb-admin-style-core',  apply_filters('tfhb_admin_core_style', THB_URL .'build/assets/tfhb-admin-app.css'), [], time(), 'all');
  
  
 		wp_localize_script(
