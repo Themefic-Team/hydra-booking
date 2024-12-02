3fd2240a884614a4dca1cb5f1356bcc43ac8a817
Ashik384 <39790492+Ashik384@users.noreply.github.com>
2024-12-02 21:24:00 +0600

    = 1.0.8 - 02/12/2024 =
    = 1.0.8 - 02/12/2024 =  
    
    - Improvement: Enhanced cancellation handling for a smoother user experience.
    - Improvement: Improved the rescheduling feature for better flexibility.
    - Improvement: Optimized notification order for clarity and effectiveness.
    - Improvement: Added improvements for using attendee phone numbers as meeting locations.
    - Improvement: Refined booking frequency options for better control.
    - Improved overall system stability for a seamless experience.
    - Improvement: Improved cancellation email formatting and delivery.
    - Improvement: Enhanced functionality for sharing embed codes.
    - Fixed: Resolved issues related to booking status updates.
    - Fixed: Fixed PayPal payment processing issues.
    - Fixed: Fixed various design issues and improved overall user interface.

--- hydra-booking.php
+++ hydra-booking.php
@@ -3,7 +3,7 @@
  * Plugin Name: Hydra Booking
  * Plugin URI: https://hydrabooking.com/
  * Description: Create a booking / Appointment Form using Contact Form 7. You can insert Calendar, Time on the form and manage your booking. User can pay using WooCommerce.
- * Version: 1.0.7
+ * Version: 1.0.8
  * Author: Themefic
  * Author URI: https://themefic.com/
  * License: GPL-2.0+
@@ -22,14 +22,9 @@ class THB_INIT {
 		// DEFINE PATH
 		define( 'THB_PATH', plugin_dir_path( __FILE__ ) );
 		define( 'THB_URL', plugin_dir_url( __FILE__ ) );
-		define( 'THB_VERSION', '1.0.7' );
-
+		define( 'THB_VERSION', '1.0.8' );
 
 		
-	
-		
-
-
 		// Load Vendor Auto Load
 		if ( file_exists( THB_PATH . '/vendor/autoload.php' ) ) {
 
