252e9bc2f8e9facdc64b99651858c1f347b1018e
Jahid Hasan <jahidhasancmt7@gmail.com>
2025-02-21 15:06:56 +0600

    email template design ongoing

--- src/view/settings/Notifications.vue
+++ src/view/settings/Notifications.vue
@@ -211,6 +211,118 @@ onBeforeMount(() => {
 
 </script>
 <template> 
+
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Booking Confirmation</title>
+    </head>
+    <body style="margin: 0; padding: 0; background-color: #F4F4F4; font-family: Arial, sans-serif;">
+
+        <!-- Main Container -->
+        <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%" bgcolor="#F4F4F4">
+            <tr>
+                <td align="center">
+                    <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="600" bgcolor="#FFFFFF" style="border-radius: 8px; overflow: hidden; box-shadow: 0px 0px 10px rgba(0,0,0,0.1);">
+                        
+                        <!-- Header -->
+                        <tr>
+                            <td bgcolor="#215732" style="padding: 16px 32px; text-align: left;">
+                                <table role="presentation" cellspacing="0" cellpadding="0" border="0">
+                                    <tr>
+                                        <td style="vertical-align: middle;">
+                                            <img :src="$tfhb_url+'/assets/images/hydra-booking-logo.png'" alt="HydraBooking"  style="max-height: 36px;display: block;">
+                                        </td>
+
+                                        <td style="vertical-align: middle; padding-left: 8px;">
+                                            <span style="color: #FFF; font-size: 22px; font-weight: 600; margin: 0;">HydraBooking</span>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+
+                        </tr>
+
+                        <!-- Content -->
+                        <tr>
+                            <td style="padding: 32px;">
+                                <p style="font-weight: bold;margin: 0; font-size: 17px;">Hey Sydur,</p>
+                                <p style="font-weight: bold; margin: 8px 0 32px 0; font-size: 17px;">A new booking with Host Name was confirmed.</p>
+
+                                <!-- Meeting Details -->
+                                <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%" style="border: 2px dashed #C0D8C4; border-radius: 8px; padding: 15px; background: #FAFAFA;">
+                                    <tr>
+                                        <td style="font-weight: bold; font-size: 16px; padding-bottom: 10px;">Meeting Details</td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <table role="presentation" cellspacing="0" cellpadding="0" border="0">
+                                                <tr>
+                                                    <td style="vertical-align: middle;">
+                                                        Date & Time:
+                                                    </td>
+
+                                                    <td style="vertical-align: middle; padding-left: 8px;">
+                                                        Sunday, November 11, 2024 at 12:00 PM (UTC+06:00) Asia - Dhaka
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                    <tr><td><strong>Host:</strong> Kamrul Hasan Shovo</td></tr>
+                                    <tr><td><strong>About:</strong> Discussion about design system</td></tr>
+                                    <tr><td><strong>Description:</strong> Hydra reads your availability from all your existing calendars ensuring you never get double booked.</td></tr>
+                                    <tr><td><strong>Location:</strong> Host will initiate the meeting by Google Meet</td></tr>
+                                    <tr><td><strong>Meeting link:</strong> <a href="https://google.meet/asdfkjasdflk">https://google.meet/asdfkjasdflk</a></td></tr>
+                                    <tr>
+                                        <td style="padding-top: 10px;">
+                                            <a href="https://google.meet/asdfkjasdflk" style="background-color: #137333; color: #FFFFFF; padding: 10px 15px; border-radius: 5px; text-decoration: none; display: inline-block; font-weight: bold;">Join Google Meet</a>
+                                        </td>
+                                    </tr>
+                                </table>
+
+                                <br>
+
+                                <!-- Host Details -->
+                                <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%" style="border: 1px solid #ddd; border-radius: 5px; padding: 15px; background: #FAFAFA;">
+                                    <tr>
+                                        <td style="font-weight: bold; font-size: 16px; padding-bottom: 10px;">Host Details</td>
+                                    </tr>
+                                    <tr><td><strong>Name:</strong> Kamrul Hasan Shovo</td></tr>
+                                    <tr><td><strong>Email:</strong> jack.spar@gmail.com</td></tr>
+                                    <tr><td><strong>Phone:</strong> +89 000 123 456</td></tr>
+                                </table>
+
+                                <br>
+
+                                <!-- Instructions -->
+                                <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%" style="border: 1px solid #ddd; border-radius: 5px; padding: 15px; background: #FAFAFA;">
+                                    <tr>
+                                        <td style="font-weight: bold; font-size: 16px; padding-bottom: 10px;">Instructions</td>
+                                    </tr>
+                                    <tr><td>• Please <strong>join the event five minutes before the event starts</strong> based on your time zone.</td></tr>
+                                    <tr><td>• Ensure you have a good internet connection, a quality camera, and a quiet space.</td></tr>
+                                </table>
+                            </td>
+                        </tr>
+
+                        <!-- Footer -->
+                        <tr>
+                            <td align="center" style="font-size: 12px; padding: 15px; background: #F9F9F9; color: #777;">
+                                © 2025 HydraBooking. All rights reserved.
+                            </td>
+                        </tr>
+
+                    </table>
+                </td>
+            </tr>
+        </table>
+
+    </body>
+    </html>
+
     <div :class="{ 'tfhb-skeleton': skeleton }" class="thb-event-dashboard ">
   
         <div  class="tfhb-dashboard-heading  ">
