d801836a8bd9d15eca5abdba2aef4deac1c6d344
Ashik384 <39790492+Ashik384@users.noreply.github.com>
2025-11-09 23:27:44 +0600

    = 1.1.27 – Nov 09, 2025 =
    = 1.1.27 – Nov 09, 2025 =
    
    - Fixed: Zoom host update settings error resolved.
    - Updated: Information in readme.txt file.
    - Compatibility: Fully compatible with WordPress v10.3.
    - Improved: System stability and overall performance.

--- hydra-booking.php
+++ hydra-booking.php
@@ -3,7 +3,7 @@
  * Plugin Name: Hydra Booking — Appointment Scheduling & Booking Calendar
  * Plugin URI: https://hydrabooking.com/
  * Description: Appointment Booking Plugin with Automated Scheduling - Apple/Outlook/ Google Calendar, WooCommerce, Zoom, Fluent Forms, Zapier, Mailchimp & CRM Integration.
- * Version: 1.1.26
+ * Version: 1.1.27
  * Tested up to: 6.8
  * Author: Themefic
  * Author URI: https://themefic.com/
@@ -26,7 +26,7 @@ class THB_INIT {
 		define( 'TFHB_PATH', plugin_dir_path( __FILE__ ) );
 		define( 'TFHB_URL', plugin_dir_url( __FILE__ ) );
 
-		define( 'TFHB_VERSION', '1.1.26' );
+		define( 'TFHB_VERSION', '1.1.27' );
 		define( 'TFHB_BASE_FILE', __FILE__);
 
 
